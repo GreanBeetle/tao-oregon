83358ff81bb063ff8bc62a8fefd602d17e7f5055
Dave Lewis and John Rykken <dave+john@>
2018-04-09 09:24:32 -0700

    Add api-keys file.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "xxxx",
+  authDomain: "xxxx.firebaseapp.com",
+  databaseURL: "https://xxxx.firebaseio.com",
+  storageBucket: "xxxx.appspot.com",
+  messagingSenderId: "xxxx"
+};
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component'; 
 
 const appRoutes: Routes = [
   {
-    // path: '',
-    // component: WelcomeComponent
+    path: '',
+    component: AppComponent
   },
 
 ]
