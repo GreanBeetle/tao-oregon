103e845d0cc8cd9b8b33516734763f71215aa897
Dave Lewis and John Rykken <dave+john@>
2018-04-09 09:56:30 -0700

    Add auth service.

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+
+@Injectable()
+export class AuthenticationService {
+
+  constructor() { }
+
+}
