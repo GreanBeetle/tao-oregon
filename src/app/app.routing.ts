0e7a68a629238aa5196c8deefab7e96df7d20194
chase Bowden and dan kiss <dan@>
2018-04-09 13:38:24 -0700

    add routing and display list of places

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
+import { ListOfTrailsComponent } from './list-of-trails/list-of-trails.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AppComponent
+  },
+  {
+    path: 'trails',
+    component: ListOfTrailsComponent
   },
 
 ]
--- src/app/list-of-trails/list-of-trails.component.ts
+++ src/app/list-of-trails/list-of-trails.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Trail } from '../models/trail.model';
+import { TrailService } from '../trail.service';
+import { Router } from '@angular/router';
+import { FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'app-list-of-trails',
   templateUrl: './list-of-trails.component.html',
-  styleUrls: ['./list-of-trails.component.css']
+  styleUrls: ['./list-of-trails.component.css'],
+  providers: [TrailService]
 })
 export class ListOfTrailsComponent implements OnInit {
-
-  constructor() { }
+trails: FirebaseListObservable<any[]>
+  constructor(private trailService: TrailService, private router: Router) { }
 
   ngOnInit() {
+    this.trails = this.trailService.getTrails();
+    
   }
 
 }
