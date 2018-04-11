85d6f7c79a0b96f795fa202f4ae1f7a293bcbcdf
John Rykken <john.rykken@gmail.com>
2018-04-11 09:57:32 -0700

    Add styling to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { WriteReportComponent } from './write-report/write-report.component';
 import { ReportService } from './report.service';
 import { SearchPipe } from './search.pipe';
 import { SearchComponent } from './search/search.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
 
 
 export const firebaseConfig = {
@@ -41,7 +42,8 @@ export const firebaseConfig = {
     AirQualityComponent,
     WriteReportComponent,
     SearchPipe,
-    SearchComponent
+    SearchComponent,
+    GoogleMapComponent
 
   ],
   imports: [
